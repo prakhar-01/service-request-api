3f034bb2b3f65738b7cc8a797f79ad81b8bfa82b
Prakhar <prakhargupta0203@gmail.com>
2026-02-11 05:36:52 +0530

    Commit-- Refactored the folder structure

--- service-request-api/src/main/java/com/prakhar/servicerequest/repository/ServiceRequestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.prakhar.servicerequest.repository;
-
-import com.prakhar.servicerequest.entity.ServiceRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRequestRepository
-        extends JpaRepository<ServiceRequest, Long> {
-}
